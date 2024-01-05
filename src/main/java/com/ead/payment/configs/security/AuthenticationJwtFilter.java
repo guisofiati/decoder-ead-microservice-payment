13e6d7080c0945383880d4f4c5a47d0475041b87
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2024-01-05 10:16:26 -0300

    project setup and rabbitmq config

---
+++ src/main/java/com/ead/payment/configs/security/AuthenticationJwtFilter.java
@@ -0,0 +1,54 @@
+package com.ead.payment.configs.security;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+public class AuthenticationJwtFilter extends OncePerRequestFilter {
+
+    Logger log = LogManager.getLogger(AuthenticationJwtFilter.class);
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwt = getTokenHeader(httpServletRequest);
+            if (jwt != null && jwtProvider.validateJwt(jwt)) {
+                String userId = jwtProvider.getSubjectJwt(jwt);
+                String roles = jwtProvider.getClaimNameJwt(jwt, "roles");
+                UserDetails userDetails = UserDetailsImpl.build(UUID.fromString(userId), roles);
+                UsernamePasswordAuthenticationToken authhentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities()
+                );
+                authhentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                SecurityContextHolder.getContext().setAuthentication(authhentication);
+            }
+        } catch (Exception e) {
+            log.error("Cannot set user authentication: {}", e);
+        }
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    private String getTokenHeader(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7, headerAuth.length());
+        }
+        return null;
+    }
+}
